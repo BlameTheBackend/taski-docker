9ee9b0748c574a96583f9ea7d247e603f65a9494
Alexandr Selyunin <alexandr_selyunin04@outlook.com>
2026-04-30 13:03:42 +0400

    Add site to settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '172.19.0.1', '192.168.1.86', '127.0.0.1', '78.85.5.216']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'yandexserverlab.sytes.net']
 
 
 # Application definition
